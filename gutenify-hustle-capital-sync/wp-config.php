ed95a46e62349e63e4d8cde746a0e30d4cbe367a
fredkiptui <fredkiptui.58@gmail.com>
2023-10-12 17:12:37 +0300

    modified

--- gutenify-hustle-capital-sync/wp-config.php
+++ gutenify-hustle-capital-sync/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'gutenify-hustle-capital-sync' );
+define('DB_NAME', 'gutenify-hustle-capital-syncing');
 
 /** Database username */
 define( 'DB_USER', 'root' );
